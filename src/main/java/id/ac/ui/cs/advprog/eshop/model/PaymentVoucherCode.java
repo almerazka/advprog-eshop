b6677c61a632e1f8488aee079f867cb6e737bf4b
almerazka <almer.azka12@gmail.com>
2025-03-07 14:00:34 +0700

    [GREEN] Implement PaymentVoucherCode Class

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentVoucherCode.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentVoucherCode.java
@@ -1,17 +1,43 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import enums.PaymentStatus;
 import java.util.Map;
 
 public class PaymentVoucherCode extends Payment {
 
-
     public PaymentVoucherCode(String id, String method, Map<String, String> paymentData) {
         super(id, method, paymentData);
 
+        // Validate the voucher code and set the status accordingly
+        String voucherCode = paymentData.get("voucherCode");
+        if (isValidVoucherCode(voucherCode)) {
+            setStatus(PaymentStatus.SUCCESS.name());
+        } else {
+            setStatus(PaymentStatus.REJECTED.name());
+        }
     }
 
     private boolean isValidVoucherCode(String voucherCode) {
+        if (voucherCode == null) {
+            return false;
+        }
 
-        return false;
+        if (voucherCode.length() != 16) {
+            return false;
+        }
+
+        if (!voucherCode.startsWith("ESHOP")) {
+            return false;
+        }
+
+        String numericalPart = voucherCode.substring(5);
+        int countNumericalChars = 0;
+        for (char c : numericalPart.toCharArray()) {
+            if (Character.isDigit(c)) {
+                countNumericalChars++;
+            }
+        }
+
+        return countNumericalChars == 8;
     }
 }

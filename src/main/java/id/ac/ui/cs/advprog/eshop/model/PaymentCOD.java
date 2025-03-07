65ba8384cbcb5e0ffe24664ebc1c77c58f57f611
almerazka <almer.azka12@gmail.com>
2025-03-07 14:11:04 +0700

    [GREEN] Implement PaymentCOD Class

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentCOD.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentCOD.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import enums.PaymentStatus;
 import java.util.Map;
 
 public class PaymentCOD extends Payment {
@@ -7,10 +8,17 @@ public class PaymentCOD extends Payment {
     public PaymentCOD(String id, String method, Map<String, String> paymentData) {
         super(id, method, paymentData);
 
-
+        if (isValidPaymentData(paymentData)) {
+            setStatus(PaymentStatus.PENDING.name());
+        } else {
+            setStatus(PaymentStatus.REJECTED.name());
+        }
     }
 
     private boolean isValidPaymentData(Map<String, String> paymentData) {
-      return false;
+        String address = paymentData.get("address");
+        String deliveryFee = paymentData.get("deliveryFee");
+
+        return address != null && !address.isEmpty() && deliveryFee != null && !deliveryFee.isEmpty();
     }
 }
